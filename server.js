75b059998e99a1448669af6f721bdae20965b2c7
NovatronicaST-DEV <emanuelngola33@gmail.com>
2023-02-23 08:01:59 +0100

    new api

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const http= require('http');
-const app= require('./app');
-const port= process.env.PORT ||3000;
-const server= http.createServer(app);
-server.listen(port);
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
